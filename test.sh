8a325176e316561b578d1b49823e7c4ca6f83f2b
Stefan Eicher <stefan.eicher@gmail.com>
2024-05-01 07:39:08 +0200

    update test.sh

--- test.sh
+++ test.sh
@@ -12,13 +12,15 @@ echo "------------------" >> time_log.txt
 free -h >> time_log.txt
 echo "------------------" >> time_log.txt
 
+export M2_HOME=$(pwd)/.m2§
+
 echo "Cloning repository and checking out commit..."
 mr -rf commons-lang
 #time git clone --depth=1 --branch 056a2244 https://github.com/apache/commons-lang >> time_log.txt
 time git clone --depth=1 https://github.com/apache/commons-lang >> time_log.txt
 cd commons-lang
 
-export M2_HOME=$(pwd)/.m2
+
 
 (time mvn dependency:go-offline) 2>> time_log.txt
 (time mvn clean) 2>> time_log.txt
