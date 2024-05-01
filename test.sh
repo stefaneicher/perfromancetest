04e32178edc9cc4bf731a198bae2e0a74ea1fd81
Stefan Eicher <stefan.eicher@gmail.com>
2024-05-01 07:47:59 +0200

    update

--- test.sh
+++ test.sh
@@ -15,16 +15,19 @@ echo "------------------" >> time_log.txt
 export M2_HOME=$(pwd)/.m2
 
 echo "Cloning repository and checking out commit..."
-mr -rf commons-lang
+
 #time git clone --depth=1 --branch 056a2244 https://github.com/apache/commons-lang >> time_log.txt
-time git clone --depth=1 https://github.com/apache/commons-lang >> time_log.txt
+#rm -rf commons-lang
+#time git clone --depth=1 https://github.com/apache/commons-lang >> time_log.txt
 cd commons-lang
 
 
-
+echo "go offline"
 (time mvn dependency:go-offline) 2>> time_log.txt
+echo "clean"
 (time mvn clean) 2>> time_log.txt
 
-(time mvn clean install -o) 2>> time_log.txt
+echo "mvn clean install -o -Drat.skip=true"
+(time mvn clean install -o -Drat.skip=true) 2>> time_log.txt
 
 echo "Process completed. Time measurements saved in time_log.txt."
