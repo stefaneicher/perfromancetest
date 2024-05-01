1ff8650925f628028f15845a52de4bb110fc23e1
Stefan Eicher <stefan.eicher@gmail.com>
2024-05-01 07:50:05 +0200

    update

--- test.sh
+++ test.sh
@@ -3,31 +3,33 @@ clear
 set -x
 set -T
 
-echo "System Information:" > time_log.txt
-echo "------------------" >> time_log.txt
-uname -a >> time_log.txt
-echo "------------------" >> time_log.txt
-#lscpu >> time_log.txt
-#echo "------------------" >> time_log.txt
-#free -h >> time_log.txt
-#echo "------------------" >> time_log.txt
+initial_dir=$(pwd)
+log_file="$initial_dir/time_log.txt"
 
-export M2_HOME=$(pwd)/.m2
+echo "System Information:" > "$log_file"
+echo "------------------" >> "$log_file"
+uname -a >> "$log_file"
+echo "------------------" >> "$log_file"
+#lscpu >> "$log_file"
+#echo "------------------" >> "$log_file"
+#free -h >> "$log_file"
+#echo "------------------" >> "$log_file"
+
+export M2_HOME="$initial_dir/.m2"
 
 echo "Cloning repository and checking out commit..."
 
-#time git clone --depth=1 --branch 056a2244 https://github.com/apache/commons-lang >> time_log.txt
+#time git clone --depth=1 --branch 056a2244 https://github.com/apache/commons-lang >> "$log_file"
 #rm -rf commons-lang
-#time git clone --depth=1 https://github.com/apache/commons-lang >> time_log.txt
+#time git clone --depth=1 https://github.com/apache/commons-lang >> "$log_file"
 cd commons-lang
 
-
 echo "go offline"
-(time mvn dependency:go-offline) 2>> time_log.txt
+(time mvn dependency:go-offline >> "$log_file") 2>&1
 echo "clean"
-(time mvn clean) 2>> time_log.txt
+(time mvn clean >> "$log_file") 2>&1
 
 echo "mvn clean install -o -Drat.skip=true"
-(time mvn clean install -o -Drat.skip=true) 2>> time_log.txt
+(time mvn clean install -o -Drat.skip=true >> "$log_file") 2>&1
 
-echo "Process completed. Time measurements saved in time_log.txt."
+echo "Process completed. Time measurements saved in $log_file."
